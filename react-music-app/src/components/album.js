ec6a527fefa4d2cab99069f900cbd662f32aba0a
aditi2605 <99731059+aditi2605@users.noreply.github.com>
2022-08-01 13:25:12 +0000

    chabges in react settup file

--- react-music-app/src/components/album.js
+++ react-music-app/src/components/album.js
@@ -3,23 +3,31 @@ import React from 'react'
 function Album() {
   return (
     <div>
-      <h1>Title : Best of Arijit Singh Songs</h1>
-      <p>Release Date : </p>
+      <h1 class="titleHeading">Title : Weeknd's best albums </h1>
       <ul>
-        <li>
-           Raabta
+        <li class="listItem">
+           Kiss Land, 2013
         </li>
-        <li>
-            Zaalima
+        <li class="listItem">
+            Echoes of Slience, 2011
         </li>
-        <li>
-            Tum hi ho
+        <li class="listItem">
+            My Dear Melancholy, 2018
         </li>
-        <li>
-            Muskurane
+        <li class="listItem">
+            Startboy, 2016
         </li>
-        <li>
-            Soch Na Sake
+        <li class="listItem">
+            Dawn FM, 2022
+        </li>
+        <li class="listItem">
+            Beauty Behind the Madness, 2015
+        </li>
+        <li class="listItem">
+            After Hours, 2020
+        </li>
+        <li class="listItem">
+            Thursday, 2011
         </li>
       </ul>
     </div>
--- react-music-app/src/components/artist.js
+++ react-music-app/src/components/artist.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Artist() {
   return (
     <div>
-    <h1>Name:Arijit Singh </h1>
-    <h1>Type of Music :Indian Classical </h1>
-    <p>Introduction :Arijit Singh (born 25 April 1987) is an Indian singer and music composer. He sings predominantly in Hindi and Bengali, but has also performed in various other Indian languages.He is the recipient of a National Award and six Filmfare Awards. He is often cited as one of the best singers in the Indian music industry, and has established a huge fan base throughout South Asia, predominantly in India, Pakistan, Bangladesh and Nepal. He is also known as the "King of Playback Singing"</p>
+    <h1>Name:Weekend (Abel Makkonen Tesfaye) </h1>
+    <h1>Type of Music : Pop, hip-hop </h1>
+    <p>Introduction :Abel Makkonen Tesfaye (born February 16, 1990), known professionally as the Weeknd, is a Canadian singer-songwriter, actor and record producer.Known for his sonic versatility and dark lyricism, his music explores escapism, romance, and melancholia, and is often inspired by personal experiences.He has received numerous accolades, including four Grammy Awards, 20 Billboard Music Awards, 17 Juno Awards, six American Music Awards, two MTV Video Music Awards, and nominations for an Academy Award, a Latin Grammy Award, and a Primetime Emmy Award</p>
     </div>
   )
 }
